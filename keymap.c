f7d2bf095d92c25c607b63429c571f11e8a38083
Karl Hallsby <karl@hallsby.com>
2022-01-07 08:24:53 -0600

    Put Delete key on keyboard
    The Delete key is on the left-side, bottom-row, 3rd in from the left,
    to the left of the "left" arrow key.

--- keymap.c
+++ keymap.c
@@ -26,6 +26,9 @@
 #include "keymap_estonian.h"
 #include "keymap_belgian.h"
 #include "keymap_us_international.h"
+#include "keymap_croatian.h"
+#include "keymap_turkish_q.h"
+#include "keymap_slovak.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -65,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                                    KC_RBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,      
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TG(1),                                                                          TG(1),          KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
-    KC_BSPACE,      WEBUSB_PAIR,    KC_TRANSPARENT, KC_LEFT,        KC_RIGHT,       KC_HYPR,                                                                                                        MT(MOD_LCTL, KC_ESCAPE),KC_UP,          KC_DOWN,        KC_MEH,         KC_TRANSPARENT, TD(DANCE_1),    
+    KC_BSPACE,      WEBUSB_PAIR,    KC_DELETE,      KC_LEFT,        KC_RIGHT,       KC_HYPR,                                                                                                        MT(MOD_LCTL, KC_ESCAPE),KC_UP,          KC_DOWN,        KC_MEH,         KC_TRANSPARENT, TD(DANCE_1),    
     KC_LCTRL,       KC_BSPACE,      KC_LGUI,                        KC_LALT,        KC_ENTER,       MT(MOD_LCTL, KC_SPACE)
   ),
   [1] = LAYOUT_moonlander(
